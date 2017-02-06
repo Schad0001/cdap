ea98881e559f5f7d77226c70b4253ac19b91fe95
Tony Bach <tbach@cask.co>
2017-02-06 14:14:58 -0800

    removed programs from the entity list view as well

--- cdap-ui/app/cdap/components/EntityListView/index.js
+++ cdap-ui/app/cdap/components/EntityListView/index.js
@@ -316,6 +316,12 @@ class EntityListView extends Component {
   ) {
     let offset = (currentPage - 1) * this.pageSize;
 
+    // TODO/FIXME: hack to not display programs when filter is empty (which means all
+    // entities should be displayed). Maybe this should be a backend change?
+    if (filter.length === 0) {
+      filter = filter.concat('app', 'artifact', 'dataset', 'stream');
+    }
+
     this.setState({
       loading : true,
       selectedEntity: null
